4ae71a6d4ace643b0dbaef50da70620ca55a20d4
Anton Vynnyk <anton.vynnyk.8118@gmail.com>
2023-07-22 23:11:31 +0300

    create: home page

--- client/src/modules/Header/Header.tsx
+++ client/src/modules/Header/Header.tsx
@@ -3,7 +3,6 @@ import {Navbar} from "./components/Navbar/Navbar.tsx";
 export const Header = () => {
 	return (
 		<div>
-			<h2>Header</h2>
 			<Navbar/>
 		</div>
 	);
---
+++ client/src/modules/Header/components/Navbar/Navbar.styled.tsx
@@ -0,0 +1,17 @@
+import styled from '@emotion/styled';
+import {NavLink} from "react-router-dom";
+
+
+export const NavbarList = styled.ul`
+	
+`;
+
+export const NavbarListLink = styled(NavLink)`
+	&.active {
+		color: #f00;
+	}
+`;
+
+export const NavbarListItem = styled.li`
+
+`;
