4504ea1060f0851dd9f57ef971cc5cc10e93c826
Anton Vynnyk <anton.vynnyk.8118@gmail.com>
2023-07-22 16:36:56 +0300

    create: base structure of client part

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,18 @@
-import './App.css';
+import {Route, Routes} from "react-router-dom";
+import {lazy} from "react";
+import {community, home} from "./shared/constants/routes.ts";
+import {Layout} from "./modules/Layout/Layout.tsx";
 
-const App = () => {
+const HomePage = lazy(() => import('./pages/HomePage/HomePage.tsx'));
+const CommunityPage = lazy(() => import('./pages/CommunityPage/CommunityPage.tsx'));
+
+export const App = () => {
 	return (
-		<>
-			<h2>LinkerFlow</h2>
-		</>
+		<Routes>
+			<Route path={home} element={<Layout/>}>
+				<Route path={home} element={<HomePage/>}/>
+				<Route path={community} element={<CommunityPage/>}/>
+			</Route>
+		</Routes>
 	);
 };
-
-export default App;
---
+++ client/src/modules/Header/Header.tsx
@@ -0,0 +1,10 @@
+import {Navbar} from "./components/Navbar/Navbar.tsx";
+
+export const Header = () => {
+	return (
+		<div>
+			<h2>Header</h2>
+			<Navbar/>
+		</div>
+	);
+};
